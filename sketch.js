1f6585a9ed094cc5f89d3dc05bedb8768e2886f7
SHREE127 <71012678+SHREE127@users.noreply.github.com>
2020-09-21 16:48:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,6 @@
 var mrect, frect;
+var rect1, rect2, rect3, rect4;
+
 
 function setup() {
   createCanvas(800,400);
@@ -8,8 +10,24 @@ function setup() {
   mrect.debug=true;
 
   frect = createSprite(400,200,40,40);
-  frect.shapeColor="red";
+  frect.shapeColor="white";
   frect.debug=true;
+
+  rect1 = createSprite(100,100,40,40);
+  rect1.shapeColor="white";
+  rect1.debug=true;
+
+  rect2 = createSprite(400,100,40,40);
+  rect2.shapeColor="white";
+  rect2.debug=true;
+
+  rect3 = createSprite(300,100,40,40);
+  rect3.shapeColor="white";
+  rect3.debug=true;
+
+  rect4 = createSprite(200,100,40,40);
+  rect4.shapeColor="white";
+  rect4.debug=true;
 
 }
 
@@ -19,17 +37,21 @@ function draw() {
   mrect.x = mouseX;
   mrect.y = mouseY;
 
-  if(mrect.x - frect.x < mrect.width/2 + frect.width/2 && frect.x - mrect.x < mrect.width/2 + frect.width/2  &&
-    ( mrect.y - frect.y < mrect.height/2 + frect.height/2 && frect.y - mrect.y < mrect.height/2 + frect.height/2)){
+ if( isTouching(rect1,mrect)||isTouching(rect2,mrect)||isTouching(rect3,mrect)||isTouching(rect4,mrect)){
+  mrect.shapeColor="green";
 
-    mrect.shapeColor="green";
-    frect.shapeColor="green";
-  }
-  else {
+ }
+ else {
 
-    mrect.shapeColor="red";
-    frect.shapeColor="red"; 
+  mrect.shapeColor="red";
 
-  }
+  frect.shapeColor="white"; 
+  rect1.shapeColor="white"; 
+  rect2.shapeColor="white"; 
+  rect3.shapeColor="white"; 
+  rect4.shapeColor="white"; 
+
+}
+
   drawSprites();
 }
